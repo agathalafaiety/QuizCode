056dbc2c8cec035ce0c6df351d73d4d69dc8b578
Agatha Lafaiety <agatha.lafaietyy@gmail.com>
2021-11-01 19:41:52 -0300

    update

--- app.js
+++ app.js
@@ -6,6 +6,34 @@ app.set('views', __dirname, 'views', 'pages');
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+    return res.render('login')
+})
+
+app.get('/cadastro', (req, res) => {
+    return res.render('cadastro')
+})
+
+app.get('/inicio', (req, res) => {
+    return res.render('inicio')
+})
+
+app.get('/configuração', (req, res) => {
+    return res.render('config')
+})
+
+app.get('/quizcode', (req, res) => {
+    return res.render('quizcodes')
+})
+
+app.get('/ranking', (req, res) => {
+    return res.render('ranking')
+})
+
+app.get('/sala', (req, res) => {
+    return res.render('sala')
+})
+
 app.use('/', userRouter);
 
 //Conexão com o servidor
---
+++ routes/configRouter.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/alterarTema', (req, res) => {
+    res.send()
+})
+
+router.get('/alterarSenha', (req, res) => {
+    res.send()
+})
+
+router.get('/devs', (req, res) => {
+    res.send()
+})
+
+module.exports = router;
